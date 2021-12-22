94813d9389992b043f05c99614400631fc40270a
Goobrr <ggoobbrr@gmail.com>
2021-12-22 12:40:26 +0700

    placeholder fx

--- src/finality/content/FinalityBlocks.java
+++ src/finality/content/FinalityBlocks.java
@@ -3,6 +3,7 @@ package finality.content;
 import finality.type.*;
 import mindustry.content.*;
 import mindustry.entities.bullet.*;
+import mindustry.gen.*;
 import mindustry.world.*;
 import mindustry.world.blocks.defense.turrets.*;
 import mindustry.world.consumers.*;
@@ -16,16 +17,28 @@ public class FinalityBlocks {
         snowfall = new PowerTurret("snowfall"){{
             buildVisibility = BuildVisibility.shown;
             size = 2;
-            reloadTime = 150f;
+            reloadTime = 75f;
             range = 120f;
             health = 200 * size * size;
+
+            inaccuracy = 0;
+            velocityInaccuracy = 0;
+
+            shootSound = Sounds.spark;
 
             shootType = new BeamBulletType(){{
                 damage = 150;
                 status = StatusEffects.freezing;
                 statusDuration = 60 * 5f;
 
+                splashDamage = 20;
+                splashDamageRadius = 30;
+
                 trailEffect = FinalityFx.cryoBeam;
+                shootEffect = FinalityFx.cryoShot;
+
+                hitEffect = Fx.none;
+                despawnEffect = FinalityFx.cryoDespawn;
             }};
 
             consumes.liquid(Liquids.cryofluid, 0.3f).update(false);
@@ -34,16 +47,28 @@ public class FinalityBlocks {
         winter = new PowerTurret("winter"){{
             buildVisibility = BuildVisibility.shown;
             size = 3;
-            reloadTime = 400f;
+            reloadTime = 200f;
             range = 200f;
             health = 300 * size * size;
+
+            inaccuracy = 0;
+            velocityInaccuracy = 0;
+
+            shootSound = Sounds.shotgun;
 
             shootType = new BeamBulletType(){{
                 damage = 250;
                 status = StatusEffects.freezing;
                 statusDuration = 60 * 10f;
 
+                splashDamage = 50;
+                splashDamageRadius = 40;
+
                 trailEffect = FinalityFx.cryoBeam;
+                shootEffect = FinalityFx.cryoShot;
+
+                hitEffect = FinalityFx.cryoSplash;
+                despawnEffect = FinalityFx.cryoDespawn;
             }};
 
             consumes.liquid(Liquids.cryofluid, 0.5f).update(false);
--- src/finality/content/FinalityFx.java
+++ src/finality/content/FinalityFx.java
@@ -1,18 +1,64 @@
 package finality.content;
 
+import arc.graphics.*;
 import arc.graphics.g2d.*;
+import arc.math.*;
 import arc.math.geom.*;
 import arc.util.*;
 import mindustry.content.*;
 import mindustry.entities.*;
+import mindustry.graphics.*;
 
 public class FinalityFx {
     public static Effect
 
     cryoBeam = new Effect(60, e -> {
-        Lines.stroke(3 * e.fout(), Liquids.cryofluid.color);
+        Draw.z(Layer.bullet - 1);
+        Draw.blend(Blending.additive);
+
+        Lines.stroke(4 * e.fout(Interp.pow5In), Liquids.cryofluid.color);
+
         if(e.data instanceof Vec2 v){
             Lines.line(e.x, e.y, v.x, v.y);
         }
+
+        Draw.blend();
+    }),
+
+    cryoShot = new Effect(30, e -> {
+        Draw.color(Liquids.cryofluid.color);
+        float w = 10 * e.fout(Interp.pow5In);
+
+        Drawf.tri(e.x, e.y, w, 50, e.rotation);
+        Drawf.tri(e.x, e.y, w, 10, e.rotation - 180);
+
+        Draw.z(Layer.effect + 0.1f);
+        Draw.blend(Blending.additive);
+
+        Drawf.tri(e.x, e.y, w * 1.5f, 55, e.rotation);
+        Drawf.tri(e.x, e.y, w * 1.5f, 15, e.rotation - 180);
+
+        Draw.blend();
+    }),
+
+    cryoDespawn = new Effect(60, e -> {
+        Draw.z(Layer.bullet - 0.1f);
+        Draw.blend(Blending.additive);
+
+        Tmp.c1.set(Liquids.cryofluid.color).a(e.fout(Interp.pow5In));
+        Fill.light(e.x, e.y, 10, 20, Tmp.c1, Color.clear);
+
+        Draw.blend();
+    }),
+
+    cryoSplash = new Effect(60, e -> {
+        float rad = 40 * e.fin(Interp.pow5Out);
+        Draw.z(Layer.bullet - 0.1f);
+        Draw.blend(Blending.additive);
+
+        Tmp.c1.set(Liquids.cryofluid.color).a(e.fout(Interp.pow5In));
+        Fill.light(e.x, e.y, Lines.circleVertices(rad), rad, Color.clear, Tmp.c1);
+
+        Draw.blend();
     });
 }
